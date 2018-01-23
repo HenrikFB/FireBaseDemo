1f7deddc2fd46b03f65ba74129bac13ea8b21fac
Henrik Fog Bunzel <henrik_1993@hotmail.com>
2018-01-23 12:20:11 +0100

    Cleaned up in the old code and ready to start. Layout created

--- app/src/main/java/com/example/henrikfogbunzel/newfireapplication/MainActivity.java
+++ app/src/main/java/com/example/henrikfogbunzel/newfireapplication/MainActivity.java
@@ -2,79 +2,16 @@ package com.example.henrikfogbunzel.newfireapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import org.w3c.dom.Text;
-
-import java.util.ArrayList;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
 
-    private ListView listView;
-    private FirebaseDatabase database;
-    private DatabaseReference myRef;
-    private ArrayList<String> mUserName = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("Users");
 
-        listView = (ListView) findViewById(R.id.newListView);
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,mUserName);
-        listView.setAdapter(arrayAdapter);
-        myRef.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                String value = dataSnapshot.getValue(String.class);
-                mUserName.add(value);
-                arrayAdapter.notifyDataSetChanged();
-
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
     }
 
-    public void buttonClicked(View view){
-        //EditText editTextName = (EditText) findViewById(R.id.name);
-        //EditText editTextContact = (EditText) findViewById(R.id.contact);
-        //myRef = database.getReference();
-        //myRef.child("Name").setValue(editTextName.getText().toString());
-        //myRef.child("Contact").setValue(editTextContact.getText().toString());
-    }
 }
