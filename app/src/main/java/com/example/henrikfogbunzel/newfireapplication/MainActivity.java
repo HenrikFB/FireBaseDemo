36641cc081e65d7a1ce2ac85f6a216057088594c
Henrik Fog Bunzel <henrik_1993@hotmail.com>
2018-01-23 14:34:38 +0100

    Email and pass auth.. Not working

--- app/src/main/java/com/example/henrikfogbunzel/newfireapplication/MainActivity.java
+++ app/src/main/java/com/example/henrikfogbunzel/newfireapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.henrikfogbunzel.newfireapplication;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -28,7 +29,54 @@ public class MainActivity extends AppCompatActivity {
     private EditText emailText;
     private EditText passText;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        emailText = (EditText) findViewById(R.id.userEmail);
+        passText = (EditText) findViewById(R.id.userPass);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser()!=null){
+                    Intent intent = new Intent(MainActivity.this, NewActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(authStateListener);
+    }
+
+    public void loginButtonClicked(View view){
+        String email = emailText.getText().toString();
+        String pass = passText.getText().toString();
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(pass)){
+            Toast.makeText(this, "Enter both values", Toast.LENGTH_LONG).show();
+        }
+        else{
+            mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful()){
+                        Toast.makeText(MainActivity.this, "Incorrect username or password", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+    }
+}
+/*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -77,3 +125,5 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
+*/
