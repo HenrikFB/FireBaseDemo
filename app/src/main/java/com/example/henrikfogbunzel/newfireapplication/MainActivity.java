a9f9259de60cc2bcc1a375d50198b19e196b75a2
Henrik Fog Bunzel <henrik_1993@hotmail.com>
2018-01-23 14:11:20 +0100

    Email and pass auth.. but not working.

--- app/src/main/java/com/example/henrikfogbunzel/newfireapplication/MainActivity.java
+++ app/src/main/java/com/example/henrikfogbunzel/newfireapplication/MainActivity.java
@@ -1,10 +1,32 @@
 package com.example.henrikfogbunzel.newfireapplication;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    private EditText emailText;
+    private EditText passText;
 
 
     @Override
@@ -12,6 +34,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        emailText = (EditText) findViewById(R.id.userEmail);
+        passText = (EditText) findViewById(R.id.userPass);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser()!= null) {
+                    Intent intent = new Intent(MainActivity.this, NewActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(authStateListener);
+    }
+
+    public void loginButtonClicked(View view){
+        String email = emailText.getText().toString();
+        String pass = passText.getText().toString();
+
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(pass)){
+            Toast.makeText(this, "Enter both Values", Toast.LENGTH_LONG).show();
+        }
+        else {
+            mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful()){
+                        Toast.makeText(MainActivity.this, "Incorrect username or password", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
     }
 
 }
